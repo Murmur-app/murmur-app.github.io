76941dc24c07b5e043a886d61c0bbb24fb0bd0de
Vatsalya <vatsalya_prakash@mckinsey.com>
2017-01-12 02:12:48 +0530

    added basic color and play/pause functionaity

--- app/scripts/audioScript.js
+++ app/scripts/audioScript.js
@@ -16,7 +16,7 @@ const audioWrapper = $('.audioSection');
 
 audioJson.forEach((obj, index) => {
   let audioImage = '<img src="./../assets/images/play-button.png" class="audioImage audioImage'+ index +'" />';
-  let audioSlider = '<div></div>';
+  let audioSlider = '<div id="slider" class="slider"><input id="slider-input1" class="slider-input1" type="range" min="0" value="0" max="100" /></div>';
   let audioName = '<div class="audioName audioName'+ index +'">'+ obj.name +'</div>'
   let audio = '<audio class="audio audio'+ index +' hidden" controls><source src="./../assets/audio/Ocean.mp3" type="audio/mpeg"></audio>';
   let audioObjTemplate = '<div class="audioContainer" onClick="playAudio('+ index +')">'+ audioImage + audio + audioSlider + audioName +'</div>';
@@ -28,5 +28,25 @@ const playAudio = (audioIndex) => {
   let clickedAudio = $.grep(audioJson, (obj) => {
     return obj.id === audioIndex;
   })
+  var currAudio = $('.audio' + audioIndex)[0];
+  if (currAudio.paused) {
+    currAudio.play();
+  } else {
+    currAudio.pause();
+  }
   console.log(clickedAudio[0]);
 }
+
+
+$('#randomPlay').on('click', function() {
+  var currAudio = $('#audio1')[0];
+  if (currAudio.paused) {
+    $('#randomPlay').removeClass('play');
+    $('#randomPlay').addClass('pause');
+    currAudio.play();
+  } else {
+    $('#randomPlay').removeClass('pause');
+    $('#randomPlay').addClass('play');
+    currAudio.pause();
+  }
+});
