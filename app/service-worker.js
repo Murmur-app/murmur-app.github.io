5733a84eb0d947942e7f12185b0d515b3075c0a0
Himanshu Singh <himanshusingh2407@gmail.com>
2017-03-18 01:28:18 +0530

    Added Service worker

--- app/service-worker.js
+++ app/service-worker.js
@@ -3,6 +3,24 @@ var cacheName = 'murmurPWA-final-1';
 var filesToCache = [
   '/',
   '/index.html',
-  '/scripts/app.js',
-  '/styles/style.css'
+  '/scripts/main.js'
 ];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
