8b853948777f682067becf96e63a09e1eb025432
Himanshu Singh <himanshusingh2407@gmail.com>
2017-03-18 16:34:24 +0530

    Added File Listing in service worker

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+window.addEventListener('load', function() {
+  navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+});
+}
+
 var convertToInlineSvg = function(doThis) {
   $('img.svg').each(function() {
     var $img = jQuery(this);
